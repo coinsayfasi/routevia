f237f3c66a90f00a9822e88161822a5fe3a9c07a
coinsayfasi <79537099+coinsayfasi@users.noreply.github.com>
2026-03-13 13:13:50 +0300

    Remove stale legal import

--- apps/mobile/lib/src/features/trips/community_post_editor_screen.dart
+++ apps/mobile/lib/src/features/trips/community_post_editor_screen.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import '../../core/constants.dart';
 import '../../core/error_utils.dart';
